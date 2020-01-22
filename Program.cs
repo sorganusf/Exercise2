ed1a5ddd89886cc25edf9e338bc816de89bec401
sorganusf <54554261+sorganusf@users.noreply.github.com>
2020-01-21 20:57:45 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Exercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int age;
+            int height;
+            //Max Heart rate is equal to 220 - age
+
+            age = 25;
+            height = 72;
+
+            Console.WriteLine("I am 25 years old, my max heart is 195 and I am 72 inches tall.");
+            Console.ReadLine();
+        }
+    }
+}
